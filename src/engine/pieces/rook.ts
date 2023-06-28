de3c8d44f44ae4461f61482d0956a108b8319b2a
Arina Turcu <arina.turcu@softwire.com>
2023-06-28 16:56:29 +0300

    rook can move

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,33 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const moves: Square[] = [];
+        const currentPos = board.findPiece(this);
+
+        let i = 1;
+        while (currentPos.col + i < 8) {
+            moves.push(new Square(currentPos.row, currentPos.col + i));
+            i++;
+        }
+
+        i = 1;
+        while (currentPos.col - i >= 0) {
+            moves.push(new Square(currentPos.row, currentPos.col - i));
+            i++;
+        }
+
+        i = 1;
+        while (currentPos.row + i < 8) {
+            moves.push(new Square(currentPos.row + i, currentPos.col));
+            i++
+        }
+
+        i = 1;
+        while (currentPos.row - i >= 0) {
+            moves.push(new Square(currentPos.row - i, currentPos.col));
+            i++
+        }
+
+        return moves;
     }
 }

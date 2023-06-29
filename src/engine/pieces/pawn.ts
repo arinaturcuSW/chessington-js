4f83a6b6c1acdc55dda188f592677e04717978f7
Arina Turcu <arina.turcu@softwire.com>
2023-06-29 12:04:54 +0300

    pawns can take pieces

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import King from "./king";
 
 export default class Pawn extends Piece {
     private hasMoved: boolean;
@@ -19,6 +20,20 @@ export default class Pawn extends Piece {
 
         if (board.isMoveValid(currentPos.row + direction, currentPos.col) && board.isSquareAvailable(currentPos.row + direction, currentPos.col)){
             moves.push(new Square(currentPos.row + direction, currentPos.col));
+        }
+
+        // diagonal hunt
+        const squareDiagLeft = new Square(currentPos.row + direction, currentPos.col - 1);
+        const squareDiagRight = new Square(currentPos.row + direction, currentPos.col + 1);
+
+        const leftPiece = board.isMoveValid(squareDiagLeft.row, squareDiagLeft.col) ? board.getPiece(squareDiagLeft) : undefined;
+        if (leftPiece && leftPiece.player !== this.player && !(leftPiece instanceof King)) {
+            moves.push(squareDiagLeft);
+        }
+
+        const rightPiece = board.isMoveValid(squareDiagLeft.row, squareDiagLeft.col) ? board.getPiece(squareDiagRight) : undefined;
+        if (rightPiece && rightPiece.player !== this.player && !(rightPiece instanceof King)) {
+            moves.push(squareDiagRight);
         }
 
         if (

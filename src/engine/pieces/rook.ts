593ae96b5663b7cc1e71bb49490693c43131aa0b
Arina Turcu <arina.turcu@softwire.com>
2023-06-29 10:22:31 +0300

    rooks cannot move through pieces

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -12,19 +13,28 @@ export default class Rook extends Piece {
         const moves: Square[] = [];
         const currentPos = board.findPiece(this);
 
-        let distance = [1, 2, 3, 4, 5, 6, 7, 8];
+        let distance = [1, 2, 3, 4, 5, 6, 7];
         let direction = [-1, 0, 1];
+        let shouldBreak = false;
 
-        distance.forEach(dist => {
-            direction.forEach(dirRow => {
-                direction.forEach(dirCol => {
-                    if (dirCol * dirRow !== 0 || dirCol + dirRow === 0) {
+        direction.forEach(dirRow => {
+            direction.forEach(dirCol => {
+                shouldBreak = false;
+                distance.forEach(dist => {
+                    if (dirCol * dirRow !== 0 || dirCol + dirRow === 0 || shouldBreak) {
                         return;
                     }
 
-                    if (board.isMoveValid(currentPos.row + dist * dirRow, currentPos.col + dist * dirCol)) {
-                        moves.push(new Square(currentPos.row + dist * dirRow, currentPos.col + dist * dirCol));
+                    if (!board.isMoveValid(currentPos.row + dist * dirRow, currentPos.col + dist * dirCol)) {
+                        return;
                     }
+
+                    if (!board.isSquareAvailable(currentPos.row + dist * dirRow, currentPos.col + dist * dirCol)) {
+                        shouldBreak = true;
+                        return;
+                    }
+
+                    moves.push(new Square(currentPos.row + dist * dirRow, currentPos.col + dist * dirCol));
                 })
             });
         });
